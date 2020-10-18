5e0d7cfbb2bf84120060e161d6a010a602dbcfc7
Barry Day <brday@ucsc.edu>
2020-10-18 13:50:40 -0700

    Added basic movement and camera control

--- Project 2 - Character Controllers/Assets/Scripts/BillBall.cs
+++ Project 2 - Character Controllers/Assets/Scripts/BillBall.cs
@@ -6,23 +6,39 @@ public class BillBall : MonoBehaviour
 {
 
     [Range(0.1f, 10)] public float speed;
-
     [SerializeField] public CharacterController Controller;
     [SerializeField] public Camera CameraFirst;
     [SerializeField] public Camera CameraThird;
+    public int camera = 1;
 
     private void Update()
     {
-        Controller.SimpleMove(new Vector3(0, 0, speed * Input.GetAxis("Vertical")));
 
-        if (Input.GetAxis("Horizontal") == 1)
+
+        if (camera == 1)
         {
-            Debug.Log("1");
+
+            CameraFirst.depth = 1;
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                CameraFirst.transform.Rotate(0, -0.3f, 0);
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                CameraFirst.transform.Rotate(0, 0.3f, 0);
+            }
         }
-        else if (Input.GetAxis("Horizontal") == -1)
+        else
         {
-            Debug.Log("-1");
+            CameraFirst.depth = -1;
+
+            Controller.SimpleMove(new Vector3(speed * Input.GetAxis("Horizontal"), 0, speed * Input.GetAxis("Vertical")));
         }
 
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            camera *= -1;
+        }
     }
 }
