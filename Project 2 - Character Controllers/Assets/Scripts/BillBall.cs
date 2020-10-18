efd339abcde98d2fd41020e5a2ef152f43606707
Barry Day <brday@ucsc.edu>
2020-10-18 11:19:08 -0700

    Added basic scene for Project 2

---
+++ Project 2 - Character Controllers/Assets/Scripts/BillBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBall : MonoBehaviour
+{
+
+    [Range(0.1f, 10)] public float speed;
+
+    [SerializeField] public CharacterController Controller;
+    [SerializeField] public Camera CameraFirst;
+    [SerializeField] public Camera CameraThird;
+
+    private void Update()
+    {
+        Controller.SimpleMove(new Vector3(0, 0, speed * Input.GetAxis("Vertical")));
+
+        if (Input.GetAxis("Horizontal") == 1)
+        {
+            Debug.Log("1");
+        }
+        else if (Input.GetAxis("Horizontal") == -1)
+        {
+            Debug.Log("-1");
+        }
+
+    }
+}
