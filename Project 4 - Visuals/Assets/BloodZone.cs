699e80cc5a920aabef1a9afa8d4aa31875562c5f
Barry Day <brday@ucsc.edu>
2020-11-23 15:31:00 -0800

    Blood sacrifice

---
+++ Project 4 - Visuals/Assets/BloodZone.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodZone : MonoBehaviour
+{
+    public Player player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player")
+        {
+            Debug.Log("HERE");
+            StartCoroutine(Kill());
+        }
+    }
+
+    private IEnumerator Kill()
+    {
+        player.alive = false;
+        yield return new WaitForSecondsRealtime(4f);
+        Debug.Log("COMMENCE");
+    }
+}
--- Project 4 - Visuals/Assets/Player.cs
+++ Project 4 - Visuals/Assets/Player.cs
@@ -4,38 +4,74 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Transform transform;
+    //new Transform transform;
     private CharacterController controller;
-    private BoxCollider collider;
+    //new BoxCollider collider;
+    private Transform model;
     private Animator animator;
+    //private ParticleSystem bloodfire;
+    public ParticleSystem bloodfire;
+
+    public GameObject vol;
 
     private Vector3 positionInitial;
     private Vector3 positionFinal;
+    private bool flag = false;
 
     [Range(0.1f, 100)]
     public float speed;
+    public bool alive = true;
 
-    void Start()
+    void Awake()
     {
-        transform = GetComponent<Transform>();
+        //transform = GetComponent<Transform>();
         controller = GetComponent<CharacterController>();
-        collider = GetComponent<BoxCollider>();
-        animator = transform.GetComponentsInChildren<Animator>()[0];
+        //collider = GetComponent<BoxCollider
+        model = transform.GetChild(0);
+        animator = GetComponentsInChildren<Animator>()[0];
+        //bloodfire = GetComponentsInChildren<ParticleSystem>()[1];
     }
 
     void Update()
     {
 
-        var delta = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
-        controller.SimpleMove(delta);
 
-        if (delta.magnitude > 0.1)
+        if (alive)
         {
-            animator.Play("WalkState");
+            var delta = new Vector3(Input.GetAxis("Horizontal") * speed, 0, Input.GetAxis("Vertical") * speed);
+            controller.SimpleMove(delta);
+
+            if (delta.magnitude > 0.1)
+            {
+                animator.Play("WalkState");
+            }
+            else
+            {
+                animator.Play("IdleState");
+            }
         }
         else
         {
-            animator.Play("IdleState");
+            if (!flag)
+            {
+                animator.enabled = false;
+                bloodfire.Play();
+                flag = true;
+                Debug.Log("???");
+            }
+
+            StartDying();
+        }
+
+    }
+
+    private void StartDying()
+    {
+
+        if (Random.Range(1, 100) > 90)
+        {
+            var rotation = Quaternion.Euler(Random.Range(-360, 360), Random.Range(-360, 360), Random.Range(-360, 360));
+            model.localRotation = rotation;
         }
     }
 }
